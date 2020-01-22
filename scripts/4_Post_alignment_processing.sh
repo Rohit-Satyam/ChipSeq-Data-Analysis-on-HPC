470a25ecb7c1b19dddc5ae43b27564be99b94dca
Rohit Satyam <rohitsatyam102@gmail.com>
2020-01-22 12:01:33 +0530

    Create 4_Post_alignment_processing.sh

---
+++ scripts/4_Post_alignment_processing.sh
@@ -0,0 +1,23 @@
+## Scripts Using miniconda environments
+
+# Conversion of SAM to BAM and coordinates based sorting
+ls -1 *.sam > sam_input
+
+#note: Dont run sambamba with GNU parallel as it's memory footprint is very high
+
+for file in `cat sam_input`
+do
+name=$(basename ${file} .sam)
+samtools view -Sb $file | sambamba sort > $name.bam
+done
+
+ls -1 *.bam > bam_input
+
+## Alignment_Stats
+cat bam_input | parallel "samtools flagstat {}"
+
+## mark_duplicates
+for file in `cat bam_input`
+name=$(basename ${file} .bam)
+picard MarkDuplicates I=$file O=$name.bam M=$name.marked_dup_metrics.txt ASSUME_SORT_ORDER=coordinate TAGGING_POLICY=All
+done
